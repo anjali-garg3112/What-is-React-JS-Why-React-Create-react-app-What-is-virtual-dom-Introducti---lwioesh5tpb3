cb82a36d9cd754b57baf147f244f1ef34a6e7af9
anjali-garg3112 <87825069+anjali-garg3112@users.noreply.github.com>
2022-07-14 16:56:21 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,11 @@
 import React, { useEffect, useState } from "react";
 import '../styles/App.css';
+import Welcome from './Welcome';
 
 const App = () => {
 
-  let [count, setCount] = useState(0)
-
   return (
-    <div class="ball">
-      <h1 class="count" ondoubleclick={() => { alert("cant edit it") }}>{count}</h1>
-      <button class='increment-button' onclick={() => { setCount(count + 1) }}>Increment</button>
-    </div>
+ <Welcome name="Anjali Garg" />
   )
 }
 
